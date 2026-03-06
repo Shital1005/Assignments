09cc6075e8e47955c5635dc17d3d7e5991c71a74
Shital1005 <shitalmate1005@gmail.com>
2026-03-06 09:18:47 +0530

    Assignments

---
+++ javascript-training/variable.js
@@ -0,0 +1,12 @@
+console.log("welocme ")
+
+//let ,var and const  are the keyword use for variable
+
+//var are not used because it is function scope
+// let and const are used 
+//const are used when value are constant
+// there are 4 parameters on which these three keyword are differentaite
+//1.initialiatization
+//2.redecleration
+//3.reinitatialization
+//4.
---
+++ javascript-training/withoutfuntion.js
@@ -0,0 +1,37 @@
+console.log("launch the Url");
+console.log("enter the Username");
+console.log("enter the password");
+console.log("verify the launch browser");
+console.log("verify the launch page");
+console.log("verify the bank account number");
+console.log("click on logout button");
+console.log("Verify the app log out successfully");
+
+//2.To verify the bank balance
+console.log("launch the Url");
+console.log("enter the Username");
+console.log("enter the password");
+console.log("verify the launch browser");
+console.log("verify the launch page");
+console.log("verify the bank account balance");
+console.log("click on logout button");
+console.log("Verify the app log out successfully");
+
+//3.To verify the bank account number
+console.log("launch the Url");
+console.log("enter the Username");
+console.log("enter the password");
+console.log("verify the launch browser");
+console.log("verify the launch page");
+console.log("verify the bank account name");
+console.log("click on logout button");
+console.log("Verify the app log out successfully");
+
+console.log("launch the chrome browser");
+console.log("enter the url www.payroll.com");
+console.log("enter the user name");
+console.log("enter the password");
+console.log("to verify the homepage");
+console.log("to verify the emp details");
+console.log("click on lohout button");
+console.log("to verify the system log out successfully")
